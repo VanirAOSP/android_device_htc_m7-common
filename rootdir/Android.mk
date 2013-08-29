40d822cf0ac0945f83cad516a74161c2d100d4c1
Trevin Gandhi <sonicxml@gmail.com>
2013-08-29 15:52:58 -0400

    Add separate fstab for vzw devices
    Sprint and VZW devices have different mount points, and this causes all sorts of borkedness when trying to use m7spr mounts on m7vzw
    
    Change-Id: I437419d97e209ec539ea4d4005e6669185b6c326

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -24,14 +24,40 @@ LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/remount.qcom.gsm
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+endif
 
-else  # Non-GSM variants
-
+ifneq ($(filter m7spr,$(TARGET_DEVICE)),)  # Sprint 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= fstab.qcom
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/fstab.qcom.cdma
+LOCAL_SRC_FILES		:= etc/fstab.qcom.sprint
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.target.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.target.rc.cdma
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= remount.qcom
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/remount.qcom.cdma
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+endif
+
+ifneq ($(filter m7vzw,$(TARGET_DEVICE)),)  # VZW 
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= fstab.qcom
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/fstab.qcom.vzw
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
